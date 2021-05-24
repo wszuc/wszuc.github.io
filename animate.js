322b5979a2fd89d1fc6b55802429238093cb1ad0
wszuc <wiktor.szuca@gmail.com>
2021-05-24 13:02:04 +0200

    Fixed unexpected behaviour

--- animate.js
+++ animate.js
@@ -1,78 +1,93 @@
-import anime from '/node_modules/animejs/lib/anime.es.js';
+import anime from "/node_modules/animejs/lib/anime.es.js";
 
 // HERO ANIMATIONS
 
 const colorAppearHero = anime({
-    targets: '.hero .animation-left',
-    width: '100%',
-    easing: 'cubicBezier(0.895, 0.030, 0.685, 0.220)',
+  targets: ".hero .animation-left",
+  width: "100%",
+  easing: "cubicBezier(0.895, 0.030, 0.685, 0.220)",
 });
 
 const blindDissapearHero = anime({
-    targets: '.hero .blind',
-    width: '0%',
-    easing: 'cubicBezier(0.895, 0.030, 0.685, 0.220)',
+  targets: ".hero .blind",
+  width: "0%",
+  easing: "cubicBezier(0.895, 0.030, 0.685, 0.220)",
 });
 
 const headerAppears = anime({
-    targets: '.hero .text',
-    filter: 'opacity(1)',
-    easing: 'linear',
-    delay: 1500 // increase delay by 100ms for each elements.
+  targets: ".hero .text",
+  filter: "opacity(1)",
+  easing: "linear",
+  delay: 1500, // increase delay by 100ms for each elements.
 });
 
 // CONTACT SECTION ANIMATION
 
-function fireContactAnim(){
-    const colorAppearContact = anime({
-        targets: '#contact .no-blind',
-        height: '100%',
-        easing: 'cubicBezier(0.895, 0.030, 0.685, 0.220)',   
-    });
-    
-    const blindDissapearContact = anime({
-        targets: '#contact .blind',
-        height: '0vh',
-        easing: 'cubicBezier(0.895, 0.030, 0.685, 0.220)',   
-    });
-  
+function fireContactAnim() {
+  const colorAppearContact = anime({
+    targets: "#contact .no-blind",
+    height: "100%",
+    easing: "cubicBezier(0.895, 0.030, 0.685, 0.220)",
+  });
+
+  const blindDissapearContact = anime({
+    targets: "#contact .blind",
+    height: "0vh",
+    easing: "cubicBezier(0.895, 0.030, 0.685, 0.220)",
+  });
 }
 
 // TRANSITIONS - BLURRING & About section
 
-const aboutSection = document.querySelector('#about');
-const aboutOpenButton = document.querySelector('.about-button-open');
-const aboutCloseButton = document.querySelector('.about-button-close');
-const body = document.querySelector('body');
-aboutSection.style.display = 'none';
+const aboutSection = document.querySelector("#about");
+const aboutOpenButton = document.querySelector(".about-button-open");
+const aboutCloseButton = document.querySelector(".about-button-close");
+const body = document.querySelector("body");
+aboutSection.style.display = "none";
 
-aboutOpenButton.addEventListener('click', showAbout);
-aboutCloseButton.addEventListener('click', showAbout);
+aboutOpenButton.addEventListener("click", showAbout);
+aboutCloseButton.addEventListener("click", closeAbout);
 
-var isOpened = Boolean(false);
-
-function showAbout(){
-    if(isOpened==false){
-        body.classList.add('modal-open');
-        anime({targets: aboutSection, easing: 'linear', update: function(){aboutSection.style.display = 'flex';}, filter: 'opacity(1)'});
-        anime({targets: '.fadeable', filter: 'blur(10px)', easing: 'linear', duration: 1500});
-        isOpened = !isOpened;
-    }
-    else{
-        body.classList.remove('modal-open');
-        anime({targets: aboutSection, easing: 'linear', update: function(){aboutSection.style.display = 'none'}});
-        anime({targets: '.fadeable', filter: 'blur(0px)', easing: 'linear', duration: 500});
-        isOpened  = !isOpened;
-    }
-    
-
+function showAbout() {
+  body.classList.add("modal-open");
+  anime({
+    targets: aboutSection,
+    easing: "linear",
+    update: function () {
+      aboutSection.style.display = "flex";
+    },
+    filter: "opacity(1)",
+  });
+  anime({
+    targets: ".fadeable",
+    filter: "blur(10px)",
+    easing: "linear",
+    duration: 1500,
+  });
 }
 
+function closeAbout() {
+  if (body.classList.contains("modal-open"))
+    body.classList.remove("modal-open");
+  anime({
+    targets: aboutSection,
+    easing: "linear",
+    update: function () {
+      aboutSection.style.display = "none";
+    },
+  });
+  anime({
+    targets: ".fadeable",
+    filter: "blur(0px)",
+    easing: "linear",
+    duration: 500,
+  });
+}
 
 /* FORM AJAXING */
 
-var form = document.querySelector('.contact-form');
-    
+var form = document.querySelector(".contact-form");
+
 async function handleSubmit(event) {
   event.preventDefault();
   var status = document.getElementById("my-form-status");
@@ -81,36 +96,39 @@ async function handleSubmit(event) {
     method: form.method,
     body: data,
     headers: {
-        'Accept': 'application/json'
-    }
-  }).then(response => {
-    status.innerHTML = "<p>Message sent!</p>";
-    form.reset()
-  }).catch(error => {
-    status.innerHTML = "<p>Oops! There was a problem sending your message :/</p>"
-  });
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "<p>Message sent!</p>";
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML =
+        "<p>Oops! There was a problem sending your message :/</p>";
+    });
 }
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
 
 /* APPEAR ON SCROLL */
 
-const offsetProjects = window.innerHeight*0.5;
+const offsetProjects = window.innerHeight * 0.5;
 let isProjectsAnimated = Boolean(false);
 
-window.addEventListener('scroll', function () {
-    if(scrollY>=offsetProjects && isProjectsAnimated==false){
-        isProjectsAnimated = true;
-        anime({targets: works, filter: 'opacity(1)', easing: 'linear'});
-    }
-})
+window.addEventListener("scroll", function () {
+  if (scrollY >= offsetProjects && isProjectsAnimated == false) {
+    isProjectsAnimated = true;
+    anime({ targets: works, filter: "opacity(1)", easing: "linear" });
+  }
+});
 
-const offsetContact = document.getElementById('works').offsetHeight*0.5 + window.innerHeight;
+const offsetContact =
+  document.getElementById("works").offsetHeight * 0.5 + window.innerHeight;
 let isContactAnimated = Boolean(false);
 
-window.addEventListener('scroll', function () {
-    console.log(scrollY)
-    if(scrollY>=offsetContact && isContactAnimated==false){
-        fireContactAnim();
-    }
-})
-
+window.addEventListener("scroll", function () {
+  console.log(scrollY);
+  if (scrollY >= offsetContact && isContactAnimated == false) {
+    fireContactAnim();
+  }
+});
